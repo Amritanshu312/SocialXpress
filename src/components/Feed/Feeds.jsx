4e0c1013dd6b37ab5ccb9c115fc2cac88f37e14a
Amritanshu312 <ffdarkrayon@gmail.com>
2024-01-12 00:05:01 +0800

    adding user collection wehn user log in

--- src/components/Feed/Feeds.jsx
+++ src/components/Feed/Feeds.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import './Feeds.css'
 import FeedInput from './FeedInput'
 import Posts from './Posts'
@@ -8,6 +8,11 @@ const Feeds = () => {
     <div className="Feed__container">
       <FeedInput />
       <Posts />
+
+      {/* <div className="post__bg" style={{
+        background: ' #00000085', zIndex: "9",
+        position: 'absolute', top: '0', left: '0', width: '100%', height: '100vh'
+      }}></div> */}
     </div>
   )
 }
--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
-import { auth, googleProvider } from "../config/firebase";
-import { signInWithPopup } from "firebase/auth";
+import React, { useContext, useEffect } from 'react'
+import { auth, db, googleProvider } from "../config/firebase";
+import { onAuthStateChanged, signInWithPopup } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 import "../styles/Auth.css"
+import { ScreenLoadingInfo } from '../contexts/screen_Loading';
+import { doc, setDoc } from 'firebase/firestore';
 
 const Auth = () => {
   const navigate = useNavigate();
+  const { setScreenLoading } = useContext(ScreenLoadingInfo)
+  useEffect(() => { onAuthStateChanged(auth, (user) => { if (user) { navigate("/") } setScreenLoading(false) }); }, [])
+
 
   const signInWithGoogle = async () => {
     try {
-      await signInWithPopup(auth, googleProvider);
-      navigate("/")
+      const userCredential = await signInWithPopup(auth, googleProvider);
+      const { displayName, email, phoneNumber, photoURL, uid } = userCredential.user
+      // navigate("/")
+      await setDoc(doc(db, "users_", userCredential.user.uid), { displayName, email, phoneNumber, photoURL, uid });
+
     } catch (err) {
       console.error(err);
     }
