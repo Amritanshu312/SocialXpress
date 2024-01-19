559fb3170523939101c554cd702455cb90a97cd5
Amritanshu312 <ffdarkrayon@gmail.com>
2024-01-19 13:56:41 +0800

    fix: Loading

--- src/contexts/screen_Loading.jsx
+++ src/contexts/screen_Loading.jsx
@@ -3,7 +3,7 @@ import { createContext, useState } from "react"
 export const ScreenLoadingInfo = createContext()
 
 const ScreenLoadingState = (props) => {
-  const [ScreenLoading, setScreenLoading] = useState(false)
+  const [ScreenLoading, setScreenLoading] = useState(true)
 
   return (
     <ScreenLoadingInfo.Provider value={{ ScreenLoading, setScreenLoading }}>
