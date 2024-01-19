400f0f5e815ad3592d1ece153909f1a98a502ab1
Amritanshu312 <ffdarkrayon@gmail.com>
2024-01-19 12:59:05 +0800

    fix: contexts

--- src/contexts/IsAddPost.jsx
+++ src/contexts/IsAddPost.jsx
@@ -1,14 +1,14 @@
 import { createContext, useState } from "react";
 
-export const IsAddPostInfo = createContext();
+export const isAddPostInfo = createContext();
 
 const IsAddPostState = (props) => {
   const [isAddPost, setIsAddPost] = useState(false);
 
   return (
-    <IsAddPostInfo.Provider value={{ isAddPost, setIsAddPost }}>
+    <isAddPostInfo.Provider value={{ isAddPost, setIsAddPost }}>
       {props.children}
-    </IsAddPostInfo.Provider>
+    </isAddPostInfo.Provider>
   );
 };
 
