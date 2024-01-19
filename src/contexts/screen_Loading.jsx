90cbf95846cb4852f4a1fa1754070fd9149cbd26
Amritanshu312 <ffdarkrayon@gmail.com>
2024-01-19 13:12:47 +0800

    fix: home is not visible

--- src/contexts/screen_Loading.jsx
+++ src/contexts/screen_Loading.jsx
@@ -3,7 +3,7 @@ import { createContext, useState } from "react"
 export const ScreenLoadingInfo = createContext()
 
 const ScreenLoadingState = (props) => {
-  const [ScreenLoading, setScreenLoading] = useState(true)
+  const [ScreenLoading, setScreenLoading] = useState(false)
 
   return (
     <ScreenLoadingInfo.Provider value={{ ScreenLoading, setScreenLoading }}>
