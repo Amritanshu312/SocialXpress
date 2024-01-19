b228730d4605e0c571db8ef4a183bf59c953321c
Amritanshu312 <ffdarkrayon@gmail.com>
2024-01-19 12:40:58 +0800

    fix

--- src/contexts/UserInfo.jsx
+++ src/contexts/UserInfo.jsx
@@ -1,34 +1,36 @@
 import { createContext, useEffect, useState } from "react";
-import { auth, db } from "../config/firebase"
+import { auth, db } from "../config/firebase";
 import { doc, getDoc } from "firebase/firestore";
 
-export const UserInfo = createContext()
+export const UserInfo = createContext();
 
 const UserState = (props) => {
-  const [info, setInfo] = useState({})
+  const [info, setInfo] = useState({});
 
-  const getuserinfo = () => {
+  const getuserinfo = async () => {
     auth.onAuthStateChanged(async function (user) {
       if (user) {
-        let LoggedIn = true
-        let datauser = await getDoc(doc(db, 'users_', user.uid))
+        let LoggedIn = true;
+        let datauser = await getDoc(doc(db, 'users_', user.uid));
 
         console.log(datauser.data());
-        const { email, emailVerified, displayName, photoURL, banner, uid } = datauser.data()
-        setInfo({ email, emailVerified, displayName, photoURL, uid, banner, LoggedIn })
+        const { email, emailVerified, displayName, photoURL, banner, uid } = datauser.data();
+        setInfo({ email, emailVerified, displayName, photoURL, uid, banner, LoggedIn });
+      } else {
+        setInfo({ LoggedIn: false });
       }
-      else {
-        setInfo({ LoggedIn: false })
-      }
-    })
-  }
+    });
+  };
 
-  useEffect(() => getuserinfo, [])
+  useEffect(() => {
+    getuserinfo();
+  }, []);
+
   return (
     <UserInfo.Provider value={{ info, setInfo }}>
       {props.children}
     </UserInfo.Provider>
-  )
-}
+  );
+};
 
-export default UserState
+export default UserState;
